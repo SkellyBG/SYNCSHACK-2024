1ad282b030060989e973e2bde8f7070f15b7d57f
Raine Gu <z5482697@ad.unsw.edu.au>
2024-08-24 16:54:35 +1000

    ADD: addUser

--- backend/src/lib/users/user-functions.ts
+++ backend/src/lib/users/user-functions.ts
@@ -1,11 +1,13 @@
+import { v4 as uuidv4 } from 'uuid';
 import { getData, NewUser, User, Data } from '../../data/data';
 
 // Create user
 export function addUser(newUser: Omit<NewUser, 'newUserId'>): void {
   let data: Data = getData() as Data;
   let newUsers: NewUser[] = data.newUsers;
+  const newUserId: string = uuidv4();  // Generate a new UUID
   let newUserData: NewUser = {
-    newUserId: 1,
+    newUserId: newUserId,
     name: newUser.name,
     email: newUser.email,
     password: newUser.password
