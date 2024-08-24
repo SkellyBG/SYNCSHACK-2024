2650f97582c116e2fbf8a3627caa10280ac4e757
Adam Zhang <adamzhang.aus@gmail.com>
2024-08-24 17:02:36 +1000

    created requests routes folder

---
+++ backend/src/api/requests/routes.ts
@@ -0,0 +1,13 @@
+import express from "express"
+
+// create request
+
+// delete request
+// once you join a group for a subject, all other pending requests
+// for that subject should be deleted
+
+// view requests
+
+// accept requests
+
+// reject request
